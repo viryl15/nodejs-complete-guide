7f3e4ff85db8d7b1470de5167999a69bf1f940bc
viryl scott <viryl1515@gmail.com>
2022-07-20 11:46:19 +0100

    using handlebars template

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,13 @@ const products = [];
 // /admin.add-product => GET
 router.get('/add-product', (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+    res.render('add-product', {
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        formsCSS: true,
+        productCSS: true,
+    })
 })
 
 // /admin.add-product => POST
