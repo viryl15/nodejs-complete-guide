b3806e1e16c10470c5be5203e1177beb9b5076df
viryl scott <viryl1515@gmail.com>
2022-07-20 12:42:48 +0100

    using ejs template

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.engine(
-    'hbs', 
-    expressHbs({
-        layoutsDir: 'views/layouts/', 
-        defaultLayout: 'main-layout',
-        extname: 'hbs'
-    }))
-app.set('view engine', 'hbs')
+// app.engine(
+//     'hbs', 
+//     expressHbs({
+//         layoutsDir: 'views/layouts/', 
+//         defaultLayout: 'main-layout',
+//         extname: 'hbs'
+//     }))
+
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminData = require('./routes/admin')
