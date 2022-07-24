09540c00ea2caaf3b928ae759792e0b22664c777
viryl scott <viryl1515@gmail.com>
2022-07-24 09:55:05 +0100

    Understanding Sequelize (~8h)

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error')
-const db = require('./util/database')
+const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
+const Order = require('./models/order')
+const OrderItem = require('./models/order-item')
 
 const app = express();
 
@@ -19,12 +25,21 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes);
 
@@ -32,4 +47,46 @@ app.use(shopRoutes);
 
 app.use(errorController.get404)
 
-app.listen(3000);
+// Associations
+Product.belongsTo(User, 
+        {
+            constraints: true,
+            onDelete: 'CASCADE'
+        }
+    )
+User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
+Order.belongsTo(User)
+User.hasMany(Order)
+Order.belongsToMany(Product,  { through: OrderItem })
+
+sequelize
+    // .sync({force: true})
+    .sync()
+    .then(result => {
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if(!user) {
+            return User.create({ name: 'Max', email: 'test@test.com' })
+        }
+        return user
+    })
+    .then(user => {
+        // console.log("the user ==>> ", user)
+        user.getCart()
+            .then(cart => {
+                if(!cart){
+                    user.createCart()
+                }
+            })
+    })
+    .then(cart => {
+        app.listen(3000)
+    })
+    .catch(err => {
+        console.log(err)
+    })
--- controllers/admin.js
+++ controllers/admin.js
@@ -11,58 +11,97 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
     const data = req.body
     const { title, imageUrl, description, price } = data
-    const product = new Product(null, title, imageUrl, description, price)
-    product
-        .save()
-        .then(() => {
-            res.redirect('/')
-        })
-        .catch(err => console.log(err))
+    req.user.createProduct({
+        title: title, 
+        imageUrl: imageUrl, 
+        description: description, 
+        price: price
+    })
+    .then(result => {
+        console.log('Created Product')
+        res.redirect('/admin/products')
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    // const product = new Product(null, title, imageUrl, description, price)
+    // product
+    //     .save()
+    //     .then(() => {
+    //         res.redirect('/')
+    //     })
+    //     .catch(err => console.log(err))
 }
 
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit
-    console.log("req.body.productId ==>> ", req.body.productId);
+    
     if(!editMode){
         return res.redirect('/')
     }
 
     const prodId = req.params.productId
-    Product.finById(prodId, product => {
-        if (!product) {
-            return res.redirect('/')
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Add Product', 
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
+    req.user
+        .getProducts({ where: { id: prodId } })
+    // Product
+    //     .findByPk(prodId)
+        .then(products => {
+            const product = products[0]
+            if (!product) {
+                return res.redirect('/')
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Add Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            })
         })
-    })    
+        .catch(err => console.log(err))
 }
 
 exports.postEditProduct = (req, res, next) => {
     // const prodId = req.body.productId
     const data = req.body
     const { productId, title, imageUrl, description, price } = data
-    const updatedProduct = new Product(productId, title, imageUrl, description, price)
-    updatedProduct.save()
 
-    res.redirect('/admin/products')
+    Product.findByPk(productId)
+        .then(product => {
+            product.title = title
+            product.imageUrl = imageUrl
+            product.description = description
+            product.price = price
+            return product.save()
+        })
+        .then(result => {
+            res.redirect('/admin/products')
+        })
+        .catch(err => console.log(err))
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {
-            prods: products, 
-            pageTitle: 'Admin Products', 
-            path: '/admin/products'
+    // Product.findAll()
+    req.user
+        .getProducts()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products, 
+                pageTitle: 'Admin Products', 
+                path: '/admin/products'
+            })
         })
-    })
+        .catch(err => console.log(err))
 }
 
 exports.postDeleteProduct = (req, res, next) => {
         const prodId = req.body.productId
-        Product.deleteById(prodId)
-        res.redirect('/products')
+        Product.findByPk(prodId)
+                .then(product => {
+                    product.destroy()
+                })
+                .then(result => {
+                    console.log('DESTROYED PRODUCT')
+                    res.redirect('/admin/products')
+                })
+                .catch(err => console.log(err))
 }
